082e8c9888f5373e6a8ce1745a3f00b92f82475f
Shreya Patil <sp411351@gmail.com>
2026-01-17 23:26:26 +0530

    Initial commit

---
+++ password_checker.py
@@ -0,0 +1,48 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+    feedback = []
+
+    if len(password) >= 8:
+        score += 1
+    else:
+        feedback.append("Password should be at least 8 characters long.")
+
+    if re.search(r"[A-Z]", password):
+        score += 1
+    else:
+        feedback.append("Add at least one uppercase letter.")
+
+    if re.search(r"[a-z]", password):
+        score += 1
+    else:
+        feedback.append("Add at least one lowercase letter.")
+
+    if re.search(r"[0-9]", password):
+        score += 1
+    else:
+        feedback.append("Add at least one number.")
+
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+    else:
+        feedback.append("Add at least one special character.")
+
+    if score <= 2:
+        strength = "Weak"
+    elif score <= 4:
+        strength = "Moderate"
+    else:
+        strength = "Strong"
+
+    return strength, feedback
+
+password = input("Enter your password: ")
+strength, feedback = check_password_strength(password)
+
+print(f"\nPassword Strength: {strength}")
+if feedback:
+    print("Suggestions to improve your password:")
+    for tip in feedback:
+        print("-", tip)
